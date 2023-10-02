b4246baa1b7c8bdcea9843b557ff3ef800786754
ImadBKZZ <ied.boukezzata@gmail.com>
2023-10-02 17:09:10 +0200

    TP

---
+++ Driver/Include/Driver_GPIO.h
@@ -0,0 +1,29 @@
+#ifndef DRIVER_GPIO_H
+#define DRIVER_GPIO_H
+
+#include "stm32f10x.h"
+
+typedef struct {
+	GPIO_TypeDef * GPIO;
+	char GPIO_pin ;
+	char GPIO_conf ;
+} MyGPIO_struct_TypeDef;
+
+#define In_Floating 0x04
+#define In_PullDown 0x08
+#define In_PullUp 0x08
+#define In_Analog 0x00
+
+#define Out_Ppull 0x02
+#define Out_OD 0x06
+#define AltOut_Ppull 0x0A
+#define AltOut_OD 0x0E
+
+
+void MyGPIO_Init(MyGPIO_struct_TypeDef * GPIOStructPtr);
+int MyGPIO_Read(GPIO_TypeDef * GPIO , char GPIO_pin);
+void MyGPIO_set(GPIO_TypeDef * GPIO, char GPIO_pin);
+void MyGPIO_Reset(GPIO_TypeDef * GPIO, char GPIO_pin);
+void MyGPIO_Toggle(GPIO_TypeDef * GPIO, char GPIO_pin);
+
+#endif
---
+++ Driver/Source/Driver_GPIO.c
@@ -0,0 +1,98 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "stm32f10x.h"
+#include "Drive_GPIO.h"
+
+//RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN;
+
+
+void MyGPIO_Init(MyGPIO_struct_TypeDef * GPIOStructPtr){
+	
+	
+	 /*------------------Configuration Timer du GPIO---------------*/
+	
+	switch(GPIOStructPtr->GPIO){
+	
+		case GPIOA:
+			RCC->APB2ENR = RCC_APB2ENR_IOPAEN;
+		case GPIOB:
+			RCC->APB2ENR = RCC_APB2ENR_IOPBEN;
+		case GPIOC:
+			RCC->APB2ENR = RCC_APB2ENR_IOPCEN;
+		case GPIOD:
+			RCC->APB2ENR = RCC_APB2ENR_IOPDEN;
+	
+	}		
+	/*-------------------------------------------------------------*/
+	
+	
+	
+	
+	/*---------------------Configuration registre CRL/CRH---------------*/
+	
+	char CurrentPin = GPIOStructPtr->GPIO_pin;
+	char CurrentConf = GPIOStructPtr->GPIO_conf;
+	char CurrentGPIO = GPIOStructPtr->GPIO;
+	
+	if (CurrentPin < 8){
+		
+		/*---------------------Configuratio Input/Output---------------*/
+		GPIOStructPtr->GPIO->CRL &= ~(0xF << CurrentPin*4);
+		
+		switch(CurrentConf){
+			
+			case In_PullDown:
+				
+				GPIOStructPtr->GPIO->CRL |= (In_PullDown << CurrentPin*4);
+				MyGPIO_Reset(CurrentGPIO, CurrentPin);
+			
+			case In_PullUp:
+				GPIOStructPtr->GPIO->CRL |= (In_PullUp << CurrentPin*4);
+				MyGPIO_set(CurrentGPIO, CurrentPin);
+			
+			default :
+				GPIOStructPtr->GPIO->CRL |= (CurrentConf << CurrentPin*4);
+		}
+	
+	}
+	else{
+			GPIOStructPtr->GPIO->CRH &= ~(0xF << (CurrentPin%8)*4);
+			switch(CurrentConf){
+			
+			case In_PullDown:
+				
+				GPIOStructPtr->GPIO->CRH |= (In_PullDown << (CurrentPin%8)*4);
+				MyGPIO_Reset(CurrentGPIO, CurrentPin);
+			
+			case In_PullUp:
+
+				GPIOStructPtr->GPIO->CRH |= (In_PullUp << (CurrentPin%8)*4);
+				MyGPIO_set(CurrentGPIO, CurrentPin);
+			
+			default :
+
+				GPIOStructPtr->GPIO->CRH |= (CurrentConf << (CurrentPin%8)*4);
+	}
+	
+}
+
+int MyGPIO_Read(GPIO_TypeDef * GPIO , char GPIO_pin){
+
+	return (GPIO->IDR >> GPIO_pin) & 0x1;
+
+}
+
+void MyGPIO_set(GPIO_TypeDef * GPIO, char GPIO_pin){
+
+	GPIO->ODR |= (0x1 << GPIO_pin);
+}
+
+void MyGPIO_Reset(GPIO_TypeDef * GPIO, char GPIO_pin){
+
+	GPIO->ODR &= ~(1<<GPIO_pin);
+}
+
+void MyGPIO_Toggle(GPIO_TypeDef * GPIO, char GPIO_pin){
+
+
+}
